117d732d87fc6b73068bd9e647156272f85015f9
Gabriel Rufino <GabrielCRufino>
2020-01-16 12:42:21 -0300

    16/01

--- src/main/java/com/example/curso/config/TestConfig.java
+++ src/main/java/com/example/curso/config/TestConfig.java
@@ -1,7 +1,6 @@
 package com.example.curso.config;
 
 import java.time.Instant;
-
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +10,13 @@ import org.springframework.context.annotation.Profile;
 
 import com.example.curso.entities.Category;
 import com.example.curso.entities.Order;
+import com.example.curso.entities.OrderItem;
+import com.example.curso.entities.Payment;
 import com.example.curso.entities.Product;
 import com.example.curso.entities.User;
 import com.example.curso.entities.enums.OrderStatus;
 import com.example.curso.repositories.CategoryRepository;
+import com.example.curso.repositories.OrderItemRepository;
 import com.example.curso.repositories.OrderRepository;
 import com.example.curso.repositories.ProductRepository;
 import com.example.curso.repositories.UserRepository;
@@ -34,7 +36,10 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
-
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
@@ -51,6 +56,16 @@ public class TestConfig implements CommandLineRunner {
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		
 		User u1 = new User(null, "Marcos Bravo", "marcBra@gmalis.com", "988888888", "123456");
 		User u2 = new User(null, "Roberto Souca", "roberSou@gmalss.com", "977777777", "123456");
 		
@@ -60,6 +75,19 @@ public class TestConfig implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p4.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p1.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 	
--- src/main/java/com/example/curso/entities/Payment.java
+++ src/main/java/com/example/curso/entities/Payment.java
@@ -1,40 +1,42 @@
 package com.example.curso.entities;
 
 import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Set;
+import java.time.Instant;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-@Table(name = "tb_category")
-public class Category implements Serializable {
-	private static final long serialVersionUID = 1L; 
-
+@Table( name = "tb_paymant")
+public class Payment  implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String name;
+	private Instant moment;
 	
 	@JsonIgnore
-	@ManyToMany(mappedBy = "categories")
-	private Set<Product> products = new HashSet<>();
-	
-	public Category() {
+	@OneToOne
+	@MapsId
+	private Order order;
+
+	public Payment() {
 	}
 	
-	public Category(Long id ,String name) {
+
+	public Payment(Long id, Instant moment, Order order) {
 		super();
 		this.id = id;
-		this.name = name;
+		this.moment = moment;
+		this.order = order;
 	}
 
 	
@@ -44,21 +46,20 @@ public class Category implements Serializable {
 	public void setId(Long id) {
 		this.id = id;
 	}
-
-	
-	public String getName() {
-		return name;
+	public Instant getMoment() {
+		return moment;
 	}
-	public void setName(String name) {
-		this.name = name;
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+	public Order getOrder() {
+		return order;
+	}
+	public void setOrder(Order order) {
+		this.order = order;
 	}
 
-	
-	public Set<Product> getProducts() {
-		return products;
-	}
 
-	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -66,6 +67,7 @@ public class Category implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+
 
 	@Override
 	public boolean equals(Object obj) {
@@ -75,7 +77,7 @@ public class Category implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Payment other = (Payment) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -83,6 +85,5 @@ public class Category implements Serializable {
 			return false;
 		return true;
 	}
-
-	
+  
 }
